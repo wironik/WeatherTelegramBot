511a29a82a7c9b47af3da4b8cd9903d5480328cd
wironik_z <73844705+wironik@users.noreply.github.com>
2022-01-12 06:07:31 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function getMainMenu()
 		['/ping'],
 		['/time'],
 		['/WeatherNow'],
-        ['Жаба']
+        ['Жаба'],
+		['/Cube']
     ])
 }
 
@@ -26,10 +27,15 @@ function getWeatherNow()
 	//var fetch = require('node-fetch');
 	return someText
 }
+function cubeGame(ctx)
+{
+	ctx.reply('🎲')
+	ctx.deleteMessage()
+}
 
 //задаем команды боту
 bot.start((ctx) => ctx.reply(`Добро пожаловать, ${ctx.message.from.first_name}`,getMainMenu())) //ответ бота на команду /start
-bot.help((ctx) => ctx.reply('1. /ping - проверка связи\n2. /start - запуск бота\n3. /time - показывает текущее время\n4. Жаба - прикол\n5. /WeatherNow - показать погоду сейчас')) //ответ бота на команду /help
+bot.help((ctx) => ctx.reply('1. /ping - проверка связи\n2. /start - запуск бота\n3. /time - показывает текущее время\n4. Жаба - прикол\n5. /WeatherNow - показать погоду сейчас\n6. /Cube - игра "угадай число на кубе"')) //ответ бота на команду /help
 bot.command('ping', (ctx) => ctx.reply('pong')) //проверка связи
 bot.command('time', ctx => {ctx.reply(String(new Date()))}) //отображение текущего времени
 bot.hears('Жаба', ctx => {ctx.replyWithPhoto('https://memepedia.ru/wp-content/uploads/2018/07/cover1.jpg',{caption: 'It is wednesday, my dudes'})}) //присылает картинку 
@@ -37,11 +43,41 @@ bot.command('WeatherNow', async ctx=> {
 	var someText=await getWeatherNow()
 	ctx.reply(`Текущая погода: ${someText}`)
 	})
+bot.command('Cube', ctx=>{
+	ctx.reply("Выберите цифру, чтобы бросить кубик: ",
+	Markup.inlineKeyboard(
+		[Markup.button.callback('1', '1'),
+		Markup.button.callback('2', '2'),
+		Markup.button.callback('3', '3'),
+		Markup.button.callback('4', '4'),
+		Markup.button.callback('5', '5'),
+		Markup.button.callback('6', '6')])
+	.resize())
+	})
+	
+bot.action('1', ctx => {
+	cubeGame(ctx)
+	})
+bot.action('2', ctx => {
+	cubeGame(ctx)
+	})
+bot.action('3', ctx => {
+	cubeGame(ctx)
+	})
+bot.action('4', ctx => {
+	cubeGame(ctx)
+	})
+bot.action('5', ctx => {
+	cubeGame(ctx)
+	})
+bot.action('6', ctx => {
+	cubeGame(ctx)
+	})
 
 
 //обработка остального
 bot.on('text', ctx => {ctx.replyWithHTML('нет такой команды')}) //реакция на все остальные введеные сообщения
-
+bot.on('sticker', (ctx) => ctx.reply('👍'))
 
 //запуск бота
 bot.launch()
